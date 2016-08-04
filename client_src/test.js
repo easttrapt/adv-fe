451dee649bc0927ac38b4463f462aea2349dc2e3
Yuriy K <easttrapt@gmail.com>
2016-08-04 19:00:37 +0300

    hometask 1.5

---
+++ client_src/test.js
@@ -0,0 +1,43 @@
+$( document ).ready( function () {
+    var posts = Data.getPosts();
+
+    //handlesbars
+
+    Handlebars.registerHelper('json', function(posts) {
+        json = JSON.stringify(Data.getPosts(),"",4);
+        return new Handlebars.SafeString('<pre>' + Handlebars.Utils.escapeExpression(json) + '</pre>');
+    }); 
+
+    var jsonPost = $('#template').html();
+    var template = Handlebars.compile( jsonPost );
+
+    var html = template({
+        posts: Data.getPosts()
+    });
+
+    $('.posts-json').html(html);
+
+    /*---------part two--------*/
+    /*Handlebars.registerHelper('table', function ( text, options ){
+        return 'aaaa';
+    });
+
+    
+    var descriptionsPost = $('#table-template').html();
+    var tableTemplate = Handlebars.compile( descriptionsPost );
+    
+    var table = tableTemplate({
+        test: 'test'
+    });
+
+    
+    $('.posts-json').html(context);
+    */
+
+    /*---------part three--------*/
+
+    Handlebars.registerPartial('post-description', function(posts) {
+        json = JSON.stringify(Data.getPosts(),"",4);
+        return new Handlebars.SafeString('<pre>' + Handlebars.Utils.escapeExpression(json) + '</pre>');
+    });
+});
